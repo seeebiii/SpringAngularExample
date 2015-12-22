3e661eeac4ca4e6ec0c56f7ddbcffb65524c4646
Sebastian Hesse <sebi-hesse1@gmx.de>
2015-12-22 14:45:23 +0100

    Initial commit for a webapp using Spring Boot Web, JPA and JDBC

---
+++ src/main/java/de/sebastianhesse/spring_angular_example/SpringAngularExampleApplication.java
@@ -0,0 +1,12 @@
+package de.sebastianhesse.spring_angular_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAngularExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAngularExampleApplication.class, args);
+    }
+}
